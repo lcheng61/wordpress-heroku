ed6769679862c173f92cb78a1aeea2b4c2e0b5c8
Liang Cheng <liang.cheng@gmail.com>
2014-03-10 05:22:09 +0000

    change to beachstrap home.php

--- wp-content/themes/wpbootstrap/home.php
+++ wp-content/themes/wpbootstrap/home.php
@@ -1,26 +1,308 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="row">
-  <div class="span8">
-    <h1>News</h1>
 
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-  	<p><em><?php the_time('l, F jS, Y'); ?></em></p>
-  	<hr>
-    
-    
-    <?php endwhile; else: ?>
-      <p><?php _e('Sorry, there are no posts.'); ?></p>
-    <?php endif; ?>
+	<!-- mainImage -->
+ 	<div id="main">
+ 		<div class="container">
+ 			<div class="row">
+				<div class="col-lg-7 col-lg-offset-5 col-md-8 col-md-offset-4 col-sm-10 col-sm-offset-1 col-xs-12">
+					<div class="cont-wrapper">
+						<p class="intro">Beachstrap, is a new bootstrap theme, packed full of features and responsive goodness</p>
+						<p class="btn-wrp"><a class="c2a-button">Call to action</a></p>
+					</div><!-- end cont wrapper -->
+				</div><!-- end 8 -->
+ 			</div>
+ 		</div><!-- end container -->
+	</div><!-- end row -->
 
-  </div>
-  <div class="span4">
+	<!-- intro strip -->
+	<div id="intro-strip">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2"><h1>Full responsive design, CSS3 animations fully customisable</h1></div>
+			</div><!-- end row -->
+		</div><!-- end container -->
+	</div><!-- end intro strip -->
 
-    <?php get_sidebar(); ?>   
+<div id="home">
+	<div class="container">
+	<div class="row">
+		<div class="col-lg-7 col-md-7 col-sm-7 col-xs-12  text">
+			<h2>Tempor incididunt ut labore et dolore magna aliqua</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+			<p><strong>Full responsive design, css3 animationsfully customisable</strong></p>
+			<p class="btn-wrp"><a class="c2a-button">Call to action</a></p>
+		</div>
+		<div class="col-lg-5 col-md-5 col-sm-5 ipad hidden-xs">
+			<img src="images/ipadShot.jpg" class="img-responsive" alt="ipadShot" />
+		</div>
 
-  </div>
+	</div><!-- end row -->
+	</div><!-- end container -->
+</div><!-- end home -->
+
+<!-- ======================= CAROUSEL ========================= -->
+
+<div id="work">
+    <div id="myCarousel" class="carousel slide">
+    <div class="container">
+    	<div class="row">
+    		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+
+      <div class="carousel-inner">
+
+		<!-- item -->
+		<div class="item active">
+		<!-- edit contents here -->
+			<div class="container">
+				<h1>Example headline.</h1>
+				<div class="caption">
+				<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+				</div>
+				</div>
+				<img src="images/responsiveShot.png" alt="responsiveShot" class="img-responsive" >
+				<p class="btn-wrp"><a class="c2a-button">Call to action</a></p>
+				<!-- stop editing here -->
+			</div>
+	<!-- end item -->
+
+			<!-- item -->
+		<div class="item">
+		<!-- edit contents here -->
+			<div class="container">
+				<h1>Example headline.</h1>
+				<div class="caption">
+				<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+				</div>
+				</div>
+				<img src="images/responsiveShot.png" alt="responsiveShot" class="img-responsive" >
+				<p class="btn-wrp"><a class="c2a-button">Call to action</a></p>
+				<!-- stop editing here -->
+			</div>
+	<!-- end item -->
+
+	<!-- item -->
+		<div class="item">
+		<!-- edit contents here -->
+			<div class="container">
+				<h1>Example headline.</h1>
+				<div class="caption">
+				<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+				</div>
+				</div>
+				<img src="images/responsiveShot.png" alt="responsiveShot" class="img-responsive" >
+				<p class="btn-wrp"><a class="c2a-button">Call to action</a></p>
+				<!-- stop editing here -->
+			</div>
+	<!-- end item -->
+
+      </div><!-- end carousel inner -->
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+		</div>
+    	</div>
+    </div>
+    </div><!-- end carousel -->
+</div><!-- end work -->
+
+	<!-- ========================= PARALLAX BACKGROUND STRIP ================================ -->
+
+	<div id="parallax" data-speed="8" class="hidden-sm hidden-xs">
+		<div class="container">
+	 		 <div class="row">
+	 		 	<div class="col-lg-8 col-lg-offset-2">
+			 		<h2>Parallax backgrounds for an ultra modern web experience!</h2>
+	 		 	</div>
+	 		 </div>
+		 </div>
+	</div>
+
+<!-- ========================= TEAM STRIP ================================ -->
+<div id="our-team">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+				<h2>Our Team</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</p>
+			</div>
+		</div><!-- end row -->
+		<div class="row">
+			<div class="col-lg-3 col-md-3 col-sm-3">
+				<div class="staff-mem">
+					<img data-src="holder.js/210x210" class="img-responsive img-circle">
+					<h3>John Doe</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+					<p class="sn-links">
+						<a href="#" title="twitter"><i class="fa fa-twitter"></i></a>
+						<a href="#" title="facebook"><i class="fa fa-facebook"></i></a>
+						<a href="#" title="linked-in"><i class="fa fa-linkedin-square"></i></a>
+					</p>
+				</div>
+			</div><!-- end 3 -->
+
+			<div class="col-lg-3 col-md-3 col-sm-3">
+				<div class="staff-mem">
+					<img data-src="holder.js/210x210" class="img-responsive img-circle">
+					<h3>Jane Doe</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+					<p class="sn-links">
+						<a href="#" title="twitter"><i class="fa fa-twitter"></i></a>
+						<a href="#" title="facebook"><i class="fa fa-facebook"></i></a>
+						<a href="#" title="linked-in"><i class="fa fa-linkedin-square"></i></a>
+					</p>
+				</div>
+
+			</div><!-- end 3 -->
+
+			<div class="col-lg-3 col-md-3 col-sm-3">
+				<div class="staff-mem">
+					<img data-src="holder.js/210x210" class="img-responsive img-circle">
+
+					<h3>James Doe</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+					<p class="sn-links">
+						<a href="#" title="twitter"><i class="fa fa-twitter"></i></a>
+						<a href="#" title="facebook"><i class="fa fa-facebook"></i></a>
+						<a href="#" title="linked-in"><i class="fa fa-linkedin-square"></i></a>
+					</p>
+				</div>
+			</div><!-- end 3 -->
+
+			<div class="col-lg-3 col-md-3 col-sm-3">
+				<div class="staff-mem">
+					<img data-src="holder.js/210x210" class="img-responsive img-circle">
+					<h3>Joe Doe</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+					<p class="sn-links">
+						<a href="#" title="twitter"><i class="fa fa-twitter"></i></a>
+						<a href="#" title="facebook"><i class="fa fa-facebook"></i></a>
+						<a href="#" title="linked-in"><i class="fa fa-linkedin-square"></i></a>
+					</p>
+				</div>
+			</div><!-- end 3 -->
+
+		</div><!-- end row -->
+	</div><!-- end container -->
 </div>
 
+<!-- ========================= SIGN UP STRIP ================================ -->
 
-<?php get_footer(); ?>
+<div id="signup">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+				<h3>Sign up for the newsletter</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip. </p>
+
+				<div class="row">
+						<form role="form">
+							<div class="col-lg-6 col-lg-offset-2 col-md-6 col-md-offset-2 col-sm-9   col-xs-12">
+								<input type="email" class="form-control"  placeholder="Enter email">
+							</div>
+							<div class="col-lg-2 col-md-2 col-sm-3 col-xs-12"><button type="submit" class="btn btn-db">Sign up now!</button></div>
+						</form>
+				</div>
+			</div>
+		</div><!-- end row -->
+
+	</div><!-- end container -->
+</div><!-- end signup -->
+
+<!-- ========================== CONTACT =========================== -->
+
+<div id="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+					<h2 class="">Contact Us</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore. </p>
+					<form role="form">
+								<p><input class="form-control" placeholder="Enter name"></p>
+								<p><input class="form-control" placeholder="Enter email"></p>
+								<p><textarea class="form-control" rows="3"></textarea></p>
+								<p><button type="submit" class="btn btn-db">Send</button></p>
+						</form>
+				</div>
+			</div><!-- end row -->
+		</div><!-- end container -->
+</div><!-- end contact -->
+
+
+<!-- ========================== BLOG =========================== -->
+<div id="blog">
+<div class="container">
+
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12">
+				<h2>Our Blog</h2>
+		</div>
+	</div><!-- end row -->
+
+	<div class="row">
+		<div class="col-lg-4 col-md-4 col-sm-4 post">
+				<h3>Post Title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+				<div class="post-pic">
+					<img data-src="holder.js/370x240" class="img-responsive">
+				</div>
+
+					<p class="sn-links">
+						<a href="#" title="twitter"><i class="fa fa-twitter"></i></a>
+						<a href="#" title="facebook"><i class="fa fa-facebook"></i></a>
+						<a href="#" title="linked-in"><i class="fa fa-linkedin-square"></i></a>
+					</p>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 post">
+				<h3>Post Title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+				<div class="post-pic">
+					<img data-src="holder.js/370x240" class="img-responsive">
+				</div>
+
+					<p class="sn-links">
+						<a href="#" title="twitter"><i class="fa fa-twitter"></i></a>
+						<a href="#" title="facebook"><i class="fa fa-facebook"></i></a>
+						<a href="#" title="linked-in"><i class="fa fa-linkedin-square"></i></a>
+					</p>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 post">
+				<h3>Post Title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+				<div class="post-pic">
+					<img data-src="holder.js/370x240" class="img-responsive">
+				</div>
+				<p class="sn-links">
+						<a href="#" title="twitter"><i class="fa fa-twitter"></i></a>
+						<a href="#" title="facebook"><i class="fa fa-facebook"></i></a>
+						<a href="#" title="linked-in"><i class="fa fa-linkedin-square"></i></a>
+					</p>
+			</div>
+			</div><!-- end row -->
+	</div><!-- end container -->
+</div><!-- end blog -->
+
+
+</div><!-- end content -->
+
+<!-- ==================== scripts ============================ -->
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+    <script src="bootstrap/assets/js/jquery.js"></script>
+	 <script src="js/waypoints.min.js"></script>
+	 <script src="js/beachstrap-script.js"></script>
+	 <script src="js/jquery.nav.js"></script>
+	 <script src="js/jquery.scrollTo.js"></script>
+
+	 <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+	 <script src="bootstrap/assets/js/holder.js"></script>
+
+  </body>
+</html>
